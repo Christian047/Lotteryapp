29cb63526f12952f0831a47700639d9287a61856
Christian047 <126990667+Christian047@users.noreply.github.com>
2023-03-30 02:48:34 -0800

    Update Lottery_app.py

--- Lottery_app.py
+++ Lottery_app.py
@@ -1,23 +1,20 @@
-import random
 
 low = 0
-high =50
-option =30
-number = random.randint(low,high)
+high = 50
+option = 30
+number = random.randint(low, high)
 
 while True:
     guess = int(input(f"Enter a number between ({low} - {high}): "))
-    
+    # option += 1
     if guess < option:
         print(f"{guess} Sorry try again!")
 
     elif guess > option:
         print(f"{guess} sorry try again!")
-
-    else:
+    # else: 
+    if guess == option:
         
-     print(f"Congratulations, you have Won!!!", "Your lucky number is", option)
-
-     break
-
-print(f"you are a lucky winner")
+        print(f"Congratulations, you have Won!!!") 
+        break
+print(f"Your Lucky number is {option}"
